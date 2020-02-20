bd4acc6b5b763c36627a36a84b76ffc5bfc3b08c
appvinc <vincent.bataille+github@appenin.fr>
2020-02-20 18:07:03 +0100

    update et analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,6 +37,24 @@ module.exports = {
                 pageContextProperty: `menus`,
                 menus: require('./src/data/menus.json'),
             }
+        },
+        {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+                // The property ID; the tracking code won't be generated without it
+                trackingId: "UA-158841642-1",
+                // Defines where to place the tracking script
+                // `true` in the head and `false` in the body
+                head: false,
+                // Setting this parameter is optional
+                anonymize: true,
+                // Setting this parameter is also optional
+                respectDNT: true,
+                // Avoids sending pageview hits from custom paths
+                exclude: ["/preview/**", "/do-not-track/me/too/"],
+                // Delays sending pageview hits on route update (in milliseconds)
+                pageTransitionDelay: 0,
+            }
         }
     ]
 };
